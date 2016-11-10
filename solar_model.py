eaaaa781fe30223f9e132d7c9dd560e532cbe12e
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-10 20:30:16 +0300

    fixed

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx = -1*gravitational_constant*body.m*obj.m*(abs(body.x - obj.x))/(r**3)
-        body.Fy = -1*gravitational_constant*body.m*obj.m*(abs(body.y - obj.y))/(r**3)
+        body.Fx += -gravitational_constant*body.m*obj.m*(body.x - obj.x)/(r**3)
+        body.Fy += -gravitational_constant*body.m*obj.m*(body.y - obj.y)/(r**3)
 
 
 
